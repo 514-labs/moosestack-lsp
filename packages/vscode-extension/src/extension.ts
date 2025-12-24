e5557c9a8938e7eec4138de9e80aa2dcf01b9983
Dave Seleno <958603+onelesd@users.noreply.github.com>
2025-12-23 19:22:43 -0800

    use more recent vscode engine

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -56,6 +56,7 @@ export function activate(context: vscode.ExtensionContext): void {
     .get<string>('trace.server', 'off');
   const traceOutputChannel =
     vscode.window.createOutputChannel('MooseStack LSP');
+  context.subscriptions.push(traceOutputChannel);
 
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
